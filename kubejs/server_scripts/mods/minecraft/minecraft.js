f0397730859ba27e600f5508c91bd4a2d1db2240
Nuclearmace <147014683+NuclearMace@users.noreply.github.com>
2024-03-18 19:49:36 +0000

    disabled slabs to blocks to clear errorlog

--- kubejs/server_scripts/mods/minecraft/minecraft.js
+++ kubejs/server_scripts/mods/minecraft/minecraft.js
@@ -3,12 +3,14 @@ ServerEvents.recipes((event) => {
   event.shaped("4x minecraft:chest", ["XXX", "X X", "XXX"], {
     X: "#minecraft:logs"
   });
+  
   // 2x logs and 5x Iron Ingots -> 1x hopper through shaped crafting
   event.shaped("minecraft:hopper", ["TXT", "TXT", " T "], {
     T: "#forge:ingots/iron",
     X: "#minecraft:logs"
   });
-  // 2x slabs -> 1x plank through shaped crafting
+
+  /* 2x slabs -> 1x plank through shaped crafting
   event.forEachRecipe(
     { type: "minecraft:crafting_shaped", output: "#minecraft:slabs" },
     (r) => {
@@ -17,6 +19,8 @@ ServerEvents.recipes((event) => {
       event.shaped(ingredients[0], ["S", "S"], { S: output });
     }
   );
+  */
+  
   // 1x stair -> 1x plank through stonecutting
   event.forEachRecipe(
     { type: "minecraft:stonecutting", output: "#minecraft:stairs" },
